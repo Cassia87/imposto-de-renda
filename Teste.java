173e98727f7f5f9474caa0f5ae9766a629ee4699
Cassia Guedes <107877651+Cassia87@users.noreply.github.com>
2026-01-04 09:09:22 -0400

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -6,7 +6,6 @@ public class Teste {
         contribuinte1.setNomeContribuinte("Cassia");
         contribuinte1.setRendaBruta(2300.99);
         contribuinte1.setCpf("020.146.561-26");
-        contribuinte1.calculaImposto();
         System.out.println(contribuinte1.toString());
     }
 }
